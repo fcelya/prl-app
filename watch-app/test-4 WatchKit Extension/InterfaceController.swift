7871fea21fac47f220014e1b69c4aefa7508cdc6
fcelya <92804587+fcelya@users.noreply.github.com>
2022-02-22 00:18:10 -0500

    Added the watch-app

---
+++ watch-app/test-4 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,222 @@
+//
+//  InterfaceController.swift
+//  test-4 WatchKit Extension
+//
+//  Created by Library User on 2/15/22.
+//
+
+import WatchKit
+import Foundation
+import HealthKit
+
+
+class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate, HKLiveWorkoutBuilderDelegate{
+    
+    @IBOutlet var startStopButton: WKInterfaceButton!
+    @IBOutlet var bpmLabel: WKInterfaceLabel!
+    
+    //Server url
+    let serverUrl: URL = URL(string: "https://ptsv2.com/t/uf53u-1645038057/post")!
+    
+    // Our workout session
+    var session: HKWorkoutSession!
+    // Live workout builder
+    var builder: HKLiveWorkoutBuilder!
+    // Tracking our workout state
+    var state: HKWorkoutSessionState = .notStarted
+    // Access point for all data managed by HealthKit.
+    let healthStore = HKHealthStore()
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        
+        let typesToShare: Set = [
+            HKQuantityType.workoutType()
+        ]
+        
+        let typesToRead: Set = [
+            HKQuantityType.quantityType(forIdentifier: .heartRate)!,
+            HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!,
+            HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
+        ]
+        
+        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { (success, error) in if !success {
+                fatalError("Error requesting authorization from health store: \(String(describing: error)))")
+            }
+        }
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession,
+            didChangeTo toState: HKWorkoutSessionState,
+                   from fromState: HKWorkoutSessionState,
+                        date: Date){
+        print("[workoutSession] Changed State: from \(fromState.rawValue) to \(toState.rawValue)")
+    }
+    
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
+        print("[workoutSession] Encountered an error: \(error)")
+    }
+    
+    func workoutBuilder(_ workoutBuilder: HKLiveWorkoutBuilder, didCollectDataOf collectedTypes: Set<HKSampleType>) {
+        
+        var HRstringValue: String = "0"
+        var EBstringValue: String = "0"
+        var DWstringValue: String = "0"
+        let heartRateUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
+        let energyUnit = HKUnit.largeCalorie()
+        let distanceUnit = HKUnit.meter()
+        
+        
+        for type in collectedTypes {
+            guard let quantityType = type as? HKQuantityType else {
+                return // Nothing to do.
+            }
+            
+            // Calculate statistics for the type.
+            switch quantityType {
+                case HKQuantityType.quantityType(forIdentifier: .heartRate):
+                    let statistics = workoutBuilder.statistics(for: quantityType)
+                    let value = statistics!.mostRecentQuantity()?.doubleValue(for: heartRateUnit)
+                    HRstringValue = String(Int(Double(round(1 * value!) / 1)))
+                case HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned):
+                    let statistics = workoutBuilder.statistics(for: quantityType)
+                    let value = statistics!.mostRecentQuantity()?.doubleValue(for: energyUnit)
+                    EBstringValue = String(Int(Double(round(1 * value!) / 1)))
+                case HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning):
+                    let statistics = workoutBuilder.statistics(for: quantityType)
+                    let value = statistics!.mostRecentQuantity()?.doubleValue(for: distanceUnit)
+                    DWstringValue = String(Int(Double(round(1 * value!) / 1)))
+                default:
+                    return
+            }
+        }
+        DispatchQueue.main.async() { [self] in
+            if HRstringValue != "0" {
+                self.bpmLabel.setText(HRstringValue)
+            }
+            print("[workoutBuilder] Heart Rate: \(String(describing: HRstringValue))")
+            print("[workoutBuilder] Energy Burned: \(String(describing: EBstringValue))")
+            print("[workoutBuilder] Distance Walked: \(String(describing: DWstringValue))")
+            //send data to server here
+            let info: Dictionary = [
+                "HeartRate": Int(HRstringValue)!,
+                "Energy Burned": Int(EBstringValue)!,
+                "Distance Walked": Int(DWstringValue)!]
+            postHTTP(info: info as Dictionary<String, Any> ,url: serverUrl)
+        }
+    }
+    
+    func workoutBuilderDidCollectEvent(_ workoutBuilder: HKLiveWorkoutBuilder) {
+        // Retreive the workout event.
+        guard let workoutEventType = workoutBuilder.workoutEvents.last?.type else { return }
+        print("[workoutBuilderDidCollectEvent] Workout Builder changed event: \(workoutEventType.rawValue)")
+    }
+    
+    func initWorkout() {
+            let configuration = HKWorkoutConfiguration()
+            configuration.activityType = .crossTraining
+            configuration.locationType = .indoor
+            
+            do {
+                session = try HKWorkoutSession(healthStore: healthStore, configuration: configuration)
+                builder = session.associatedWorkoutBuilder()
+            } catch {
+                fatalError("Unable to create the workout session!")
+            }
+            
+            // Setup session and builder.
+            session.delegate = self
+            builder.delegate = self
+            
+            /// Set the workout builder's data source.
+            builder.dataSource = HKLiveWorkoutDataSource(healthStore: healthStore,
+                                                         workoutConfiguration: configuration)
+        }
+    
+    
+    func startWorkout() {
+            // Initialize our workout
+            initWorkout()
+            
+            // Start the workout session and begin data collection
+            session.startActivity(with: Date())
+            builder.beginCollection(withStart: Date()) { (succ, error) in
+                if !succ {
+                    fatalError("Error beginning collection from builder: \(String(describing: error)))")
+                }
+            }
+        }
+    
+    
+    func stopWorkout() {
+            // Stop the workout session
+            session.end()
+            builder.endCollection(withEnd: Date()) { (success, error) in
+                self.builder.finishWorkout { (workout, error) in
+                    DispatchQueue.main.async() {
+                        self.session = nil
+                        self.builder = nil
+                    }
+                }
+            }
+        }
+    
+    func postHTTP(info: Dictionary<String, Any>, url: URL) {
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: info, options: JSONSerialization.WritingOptions.prettyPrinted)
+            
+            var request = URLRequest(url: url)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "POST"
+            request.httpBody = jsonData
+            
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                    if let error = error {
+                        print("error=\(String(describing: error))")
+                        return
+                    }
+                    guard let httpResponse = response as? HTTPURLResponse,
+                        (200...299).contains(httpResponse.statusCode) else {
+                        print("error=\(String(describing: error))")
+                        return
+                    }
+                    if let mimeType = httpResponse.mimeType, mimeType == "text/plain",
+                        let data = data,
+                        let message = String(data: data, encoding: .utf8) {
+                        DispatchQueue.main.async {
+                            print(message)
+                        }
+                    }
+                }
+            task.resume()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+        
+    @IBAction func buttonPressed() {
+        
+        switch state{
+        case HKWorkoutSessionState.running:
+            stopWorkout()
+            state = HKWorkoutSessionState.ended
+            bpmLabel!.setText("---")
+            startStopButton!.setTitle("Start")
+        default:
+            startWorkout()
+            state = HKWorkoutSessionState.running
+            startStopButton!.setTitle("Stop")
+        }
+    }
+}
