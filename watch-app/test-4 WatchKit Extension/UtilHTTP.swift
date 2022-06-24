9ae66311630ce768f130bcb7f4d1b7ce78ea11b1
fcelya <92804587+fcelya@users.noreply.github.com>
2022-06-25 00:37:19 +0200

    Navigation between screens work

--- watch-app/test-4 WatchKit Extension/UtilHTTP.swift
+++ watch-app/test-4 WatchKit Extension/UtilHTTP.swift
@@ -51,9 +51,13 @@ extension InterfaceController{
                         case "0":
                         break
                         case "1":
+                        self.stopWorkout()
+                        self.appState = possibleAppStates.welcome
                         WKInterfaceDevice.current().play(.failure)
                         self.pushController(withName: "descansa", context: nil)
                         case "2":
+                        self.stopWorkout()
+                        self.appState = possibleAppStates.welcome
                         WKInterfaceDevice.current().play(.failure)
                         self.pushController(withName: "caida", context: nil)
                         default:
@@ -104,100 +108,3 @@ extension InterfaceController{
         return
     }
 }
-
-//extension InterfaceControllerAlert{
-//    func postHTTP2(info: Dictionary<String, Any>, url: URL) {
-//        //TODO: Return response code
-//        //create the session object
-//        let session = URLSession.shared
-//
-//        //now create the URLRequest object using the url object
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "POST" //set http method as POST
-//
-//        do {
-//            request.httpBody = try JSONSerialization.data(withJSONObject: info, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
-//
-//        } catch let error {
-//            print(error.localizedDescription)
-//        }
-//
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue("application/json", forHTTPHeaderField: "Accept")
-//
-//        //create dataTask using the session object to send data to the server
-//        let task = session.dataTask(with: request, completionHandler: { data, response, error in
-//
-//            guard error == nil else {
-//                print("[HTTP POST]: Encountered error: \(error!)")
-//                return
-//            }
-//
-//            guard let data = data else {
-//                return
-//            }
-//
-//            do {
-//                //create json object from data
-//                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-//                    print(json)
-//                    // handle json...
-//                    let emerg = json["emergency"] as! String
-//                    let prefix = String(emerg.prefix(1))
-//                    switch prefix{
-//                        case "0":
-//                        break
-//                        case "2":
-//                        WKInterfaceDevice.current().play(.failure)
-//                        self.pushController(withName: "descansa", context: nil)
-//                        case "1":
-//                        WKInterfaceDevice.current().play(.failure)
-//                        self.pushController(withName: "caida", context: nil)
-//                        default:
-//                        break
-//                    }
-//                }
-//
-//            } catch let error {
-//                print(error.localizedDescription)
-//            }
-//        })
-//        task.resume()
-//    }
-//    
-//    
-//    
-//    func sendAndSave(){
-//        //TODO: only erase data if return code is successful
-//        self.postHTTP2(info: self.workoutDict, url: self.serverUrl)
-//        self.postHTTP2(info: self.motionDict, url: self.serverUrl)
-//        self.motionDict = ["type": ["type":["motion"], "device id": [deviceID]],
-//                             "data": ["accx":[],
-//                                      "accy":[],
-//                                      "accz":[],
-//                                      "gyrx":[],
-//                                      "gyry":[],
-//                                      "gyrz":[],
-//                                      "grvx":[],
-//                                      "grvy":[],
-//                                      "grvz":[],
-//                                      "timestamp":[]]]
-//        self.workoutDict = ["type": ["type": ["health"], "device id": [deviceID]],
-//                            "data": ["Heart Rate": [],
-//                                     "Active Energy Burned": [],
-//                                     "Basal Energy Burned": [],
-//                                     "Apple Stand Time": [],
-//                                     "Apple Walking Steadiness": [],
-//                                     "Environmental Audio Exposure": [],
-//                                     "Heart Rate Variability": [],
-//                                     "Oxygen Saturation": [],
-//                                     "Body Temperature": [],
-//                                     "Blood Pressure Systolic": [],
-//                                     "Blood Pressure Dyastolic": [],
-//                                     "Respiratory Rate": [],
-//                                     "Distance Walked": []
-//                                    ]]
-//        print("[HTTP]: Sent motion and health data")
-//        return
-//    }
-//}
